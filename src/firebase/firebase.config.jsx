571af22b1625217866cee3df1656c246da1b8513
rakib <rakibhosen2244@gmail.com>
2023-09-07 05:01:40 +0600

    this is news website first commit

---
+++ src/firebase/firebase.config.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBh2GqNYHizt5eRdwIjXMePAnZeKfU5YeM",
+    authDomain: "news-portal-c44cf.firebaseapp.com",
+    projectId: "news-portal-c44cf",
+    storageBucket: "news-portal-c44cf.appspot.com",
+    messagingSenderId: "678979408386",
+    appId: "1:678979408386:web:0633089c9a7ee6676b0d1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+import Footer from "../pages/Shared/Footer/Footer";
+import Header from "../pages/Shared/Header/Header";
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
